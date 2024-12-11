8afa2d08012c511f9a6ad79298f45f129e698d44
iasssy <iana.setrakova@johnabbottcollege.net>
2024-12-11 11:22:24 -0500

    added lists to DbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UpSkillz.Models;
 
 namespace UpSkillz.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments  { get; set; }
+    public DbSet<Lesson> Lessons  { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
