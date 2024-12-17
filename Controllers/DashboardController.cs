00bc5e9c4154cd5c85be32bbb0755ceeb0c5956b
Tamara Plante <0631064@johnabbottcollege.net>
2024-12-17 13:41:31 -0500

    Add instructor view
    Enroll as an instructor or instructor create/manage courses

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -9,16 +10,18 @@ namespace UpSkillz.Controllers
 {
     public class DashboardController : Controller
     {
-       
+
         private readonly ApplicationDbContext _context;
         private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
         private ILogger<DashboardController> _logger;
 
-        public DashboardController(ApplicationDbContext context, UserManager<User> userManager, ILogger<DashboardController> logger)
+        public DashboardController(ApplicationDbContext context, UserManager<User> userManager, SignInManager<User> signInManager, ILogger<DashboardController> logger)
         {
             _context = context;
             _userManager = userManager;
+            _signInManager = signInManager;
             _logger = logger;
         }
         // GET: Courses     TODO:  for the logged user with the role Instructor or User
@@ -26,23 +29,40 @@ namespace UpSkillz.Controllers
         {
             try
             {
-                var courses = await _context.Courses.ToListAsync();
-
+                var user = await _userManager.GetUserAsync(User);
+                var courses = await _context.Courses.Where(c => c.Instructor == user).ToListAsync();
+                /*
                 if (courses == null || !courses.Any())
                 {
                     _logger.LogInformation("No courses found in the database.");
-                    return View(); // Return a view indicating no courses available
-                }
+                    return View(courses); // Return a view indicating no courses available
+                }*/
 
                 return View(courses);
             }
             catch (Exception ex)
             {
                 _logger.LogError("An error occurred while fetching courses: ", ex);
-                return View("Error"); 
+                return View("Error");
             }
         }
 
+        [Authorize]
+        public async Task<IActionResult> SignUpInstructor()
+        {
+            var user = await _userManager.GetUserAsync(User);
 
-    } 
+            if (user != null && !await _userManager.IsInRoleAsync(user, "Instructor"))
+            {
+                _logger.LogInformation("Sign up for instructor: " + user.UserName);
+
+                // Register User as instructor
+                await _userManager.AddToRoleAsync(user, "Instructor");
+                // Update cookie so it is aware of new role
+                await _signInManager.RefreshSignInAsync(user);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
 }
